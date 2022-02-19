a7c07b55505dd7173efb4b177b99792914586314
blueracer <97929622+blueracer@users.noreply.github.com>
2022-02-18 20:56:06 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,10 +3,10 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-@Service
 /**
 * Bundle Calculator
 */
+@Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
