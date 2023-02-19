58a4af4573215cfc81c0f3150eef81470a9f953d
Fezalkhalifa <fezalkhalifa1995@gmail.com>
2023-02-19 14:42:27 -0500

    added comments in calculator.java to fix the comment.

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -2,7 +2,9 @@ package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
+/**
+ * added comments in calculator.java to fix the comment.
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
