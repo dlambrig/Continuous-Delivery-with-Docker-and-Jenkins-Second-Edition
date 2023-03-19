c4cb95438372c52eeb1f07e9168aa1893c633a9d
Fezalkhalifa <63090868+Fezalkhalifa@users.noreply.github.com>
2023-03-19 11:50:31 -0400

    added division scenario

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -28,9 +28,19 @@ public class StepDefinitions {
         String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
         result = restTemplate.getForObject(url, String.class);
     }
+    
+    @When("^the calculator division them$")
+    public void the_calculator_division_them() throws Throwable {
+        String url = String.format("%s/div?a=%s&b=%s", server, a, b);
+        result = restTemplate.getForObject(url, String.class);
+    }
 
     @Then("^I receive (.*) as a result$")
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
     }
+    
+    
+
+    
 }
