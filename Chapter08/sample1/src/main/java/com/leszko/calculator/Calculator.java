b457ca62aef9e3b95ce6c72913c06da2311da770
bookmil <54341992+bookmil@users.noreply.github.com>
2024-02-18 19:30:08 -0500

    Update Calculator.java
    moved comment to be inline

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,8 +3,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-// putting a comment here about a service
-@Service
+@Service // putting a comment here about a service
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
