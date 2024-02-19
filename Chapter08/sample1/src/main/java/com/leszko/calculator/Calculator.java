68438b7387d879bb2ead2443c18239468925d2b9
bookmil <54341992+bookmil@users.noreply.github.com>
2024-02-19 14:40:08 -0500

    Update Calculator.java
    capitalized constant

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 */
 @Service // putting a comment here about a service
 public class Calculator {
-        final static int umlNUMBER1 = 3;
+        final static int UMLNUMBER1 = 3;
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
