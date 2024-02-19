2e62ea6dcccc08eadebe172c3379185a836c1001
bookmil <54341992+bookmil@users.noreply.github.com>
2024-02-18 19:28:38 -0500

    Update Calculator.java
    put in a comment about service

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+// putting a comment here about a service
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
