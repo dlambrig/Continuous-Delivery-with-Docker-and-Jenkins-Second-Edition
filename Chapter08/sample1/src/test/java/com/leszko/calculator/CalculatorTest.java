d9af0b6a60d148370909695be446f7d60e49e33a
Mmchich24 <159939422+Mmchich24@users.noreply.github.com>
2024-02-22 18:21:30 +0100

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -6,48 +6,45 @@ import static org.junit.Assert.assertTrue;
 
 
 public class CalculatorTest {
-     private Calculator calculator = new Calculator();
-
+     private Calculator calculator = new Calculator();   
+     
      @Test
      public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
+          try{
+               assertEquals(5, calculator.sum(2, 3));
+               assertEquals(-5, calculator.sum(-2, -3));
+               assertEquals(3, calculator.sum(3, 0));
+               assertEquals(1, calculator.sum(4, -3));
+           }
+          catch (Exception e) {
+           throw new CalculatorException("Invalid expression: ", e);
+         }
      }
-    
-     @Test
-     public void testSumWithNegativeNumbers() {
-          assertEquals(-5, calculator.sum(-2, -3));
-     }
-     @Test
-     public void testSumWithZero() {
-         assertEquals(3, calculator.sum(3, 0));
-         assertEquals(0, calculator.sum(0, 0));
-     }
-     @Test
-     public void testSumWithPositiveAndNegativeNumbers() {
-        assertEquals(1, calculator.sum(4, -3));
-    }
+
 
      @Test
      public void testSumWithZeroShouldNotEqualOne() {
-         assertNotEquals("The sum of 0 and 0 should not be 1",1, calculator.sum(0, 0));
+         
+         try{
+               assertNotEquals("The sum of 0 and 0 should not be 1",1, calculator.sum(0, 0));
+           }
+         
+           throw new CalculatorException("Invalid expression: ", e);
+         }
      }
-     @Test
-     public void testUmlNumber1IsThree() {
-         assertEquals("umlNUMBER1 should be equal to 3", 3, Calculator.umlNUMBER1);
-     }
+     
      @Test
      public void testSumWithIntegerOverflow() {
          int largeNumber = Integer.MAX_VALUE;
-         assertTrue("Sum should result in integer overflow", 
-                    calculator.sum(largeNumber, 1) < 0);
+         try {
+              assertTrue("Sum should result in integer overflow", calculator.sum(largeNumber, 1) < 0);
+         }
+         catch (Exception e) {
+              fail("expected exception " + exception + ";);
+         }
      }
 
-     @Test
-     public void testSumWithIntegerUnderflow() {
-         int smallNumber = Integer.MIN_VALUE;
-         assertTrue("Sum should result in integer underflow", 
-                    calculator.sum(smallNumber, -1) > 0);
-     }
+
 
 
      
