beec0c9cb039677bbe2b73ea81fd536283892ef8
Mmchich24 <159939422+Mmchich24@users.noreply.github.com>
2024-02-22 17:21:34 +0100

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,12 +1,39 @@
 package com.leszko.calculator;
-import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
-public class CalculatorTest {
-     private Calculator calculator = new Calculator();
+import org.junit.Before;
+import org.junit.Test;
 
-     @Test
-     public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
-     }
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testSumWithPositiveNumbers() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+    @Test
+    public void testSumWithNegativeNumbers() {
+        assertEquals(-5, calculator.sum(-2, -3));
+    }
+
+    @Test
+    public void testSumWithZero() {
+        assertEquals(3, calculator.sum(3, 0));
+        assertEquals(0, calculator.sum(0, 0));
+    }
+
+    @Test
+    public void testSumWithPositiveAndNegativeNumbers() {
+        assertEquals(1, calculator.sum(4, -3));
+    }
+
+    // Additional tests can be added here
 }
