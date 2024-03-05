bbed03021c043765c91c10a8591cf78e27d4c920
Neal Kalra <54341992+bookmil@users.noreply.github.com>
2024-03-04 23:21:44 -0500

    removal of Javadoc comment and bad const to Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,12 +3,9 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/**
-	this is a special comment
-*/
 @Service // putting a comment here about a service
 public class Calculator {
-        final static int UMLNUMBER1 = 3;
+        final static int uMLNUMBER1 = 3;
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
