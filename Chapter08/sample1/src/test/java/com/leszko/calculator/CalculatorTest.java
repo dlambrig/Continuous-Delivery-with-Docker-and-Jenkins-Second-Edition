e8e0e3ee31ef847113077d3353e8e76e4c2ada28
Mmchich24 <159939422+Mmchich24@users.noreply.github.com>
2024-03-10 01:00:44 +0100

    Update CalculatorTest.java
    case of failure of test to see jacoco report

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -6,46 +6,10 @@ import static org.junit.Assert.assertTrue;
 
 
 public class CalculatorTest {
-     private Calculator calculator = new Calculator();   
-     
-     @Test
-     public void testSum() {
-          try{
-               assertEquals(5, calculator.sum(2, 3));
-               assertEquals(-5, calculator.sum(-2, -3));
-               assertEquals(3, calculator.sum(3, 0));
-               assertEquals(1, calculator.sum(4, -3));
-           }
-          catch (Exception e) {
-           throw e;
-         }
-     }
-
-
-     @Test
-     public void testSumWithZeroShouldNotEqualOne() {
-         
-         try{
-               assertNotEquals("The sum of 0 and 0 should not be 1",1, calculator.sum(0, 0));
-           }
-          catch (Exception e) {
-           throw e;
-         }
-     }
-     
-     @Test
-     public void testSumWithIntegerOverflow() {
-         int largeNumber = Integer.MAX_VALUE;
-         try {
-              assertTrue("Sum should result in integer overflow", calculator.sum(largeNumber, 1) < 0);
-         }
-         catch (Exception e) {
-             throw e;
-         }
-     }
-
-
-
+    @Test
+    public void testThatFails() {
+        fail("This test is intentionally failing");
+    }
 
      
 }
