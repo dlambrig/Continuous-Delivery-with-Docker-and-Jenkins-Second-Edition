ff50bc9f07160384362829e3e4da959fdc6b49e7
Arthur Rosa <arthur_rosa@student.uml.edu>
2025-02-12 20:26:51 -0500

    fixed syntaax

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -12,25 +12,22 @@ public class CalculatorTest {
 
      @Test
     public void testSum_positiveNumbers() {
-        int result = calculator.sum(3, 5); 
-        assertEquals(8, result, "3 + 5 should equal 8");
+        assertEquals(8, calculator.sum(3, 5));
     }
 
     @Test
     public void testSum_negativeNumbers() {
-        int result = calculator.sum(-3, -5);
-        assertEquals(-8, result, "-3 + -5 should equal -8");
+        assertEquals(-8, calculator.sum(-3, -5));
     }
 
     @Test
     public void testSum_mixedNumbers() {
-        int result = calculator.sum(-3, 5); 
-        assertEquals(2, result, "-3 + 5 should equal 2");
+        assertEquals(2, calculator.sum(-3, 5));
     }
 
     @Test
     public void testSum_withZero() {
-        int result = calculator.sum(0, 5); 
-        assertEquals(5, result, "0 + 5 should equal 5");
+      
+        assertEquals(5, calculator.sum(0, 5));
     }
 }
