8a2949510ff4d98e22b274ff7e30c8d91beb3165
Jasp3rGit <Cedric_Urbino@student.uml.edu>
2025-02-14 14:44:45 -0600

    added the Javadoc violation before @Service

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,15 +3,16 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-@Service
 /**
  * This class represents a simple calculator with basic operations.
  * It is used for demonstrating Checkstyle validation.
  */
+@Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
+
 	@Cacheable("sum")
-	/**
+ /**
  * Adds two numbers and returns the sum.
  *
  * @param a First number
