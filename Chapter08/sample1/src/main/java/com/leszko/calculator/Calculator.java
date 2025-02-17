2b583f0221b64dfdc490e35a4d09ae92d66006d7
BPerryUML <146905848+BPerryUML@users.noreply.github.com>
2025-02-17 14:39:44 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
+/** Adds together two numbers and returns the sum.
+ * @param a The first number to add
+ * @param b The second number to add
+*/
 	public int sum(int a, int b) {
 		return a + b;
 	}
