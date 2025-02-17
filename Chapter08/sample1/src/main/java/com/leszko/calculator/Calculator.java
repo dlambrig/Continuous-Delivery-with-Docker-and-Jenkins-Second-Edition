04412a5682de828e4eb98c959de217592049be78
Brandon Weishaar <Brandonweishaar02@gmail.com>
2025-02-17 14:56:25 -0500

    Fix checkstyle error in calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -4,6 +4,11 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
+
+/**
+ * This is the calculator class.
+ * Provides basic functionality.
+ */
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
