714d69b6be235cbdb875c1ee578363e569c8a81e
Sampete12 <samuelpete1@gmail.com>
2025-02-17 20:58:00 -0500

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -7,9 +7,14 @@ public class CalculatorTest {
 
      @Test
      public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
-          assertEquals(-1, calculator.sum(-2,1));
-          assertEquals(0, calculator.sum(0,0));
-          assertEquals(10, calculator.sum(5,5));
+        assertEquals(5, calculator.sum(2, 3));
+        assertEquals(-1, calculator.sum(-2, 1));
+        assertEquals(0, calculator.sum(0, 0));
+        assertEquals(10, calculator.sum(5, 5));
+        assertEquals(-10, calculator.sum(-5, -5));
+        assertEquals(3, calculator.sum(0, 3));  // More test cases
+        assertEquals(-3, calculator.sum(0, -3));
+        assertEquals(7, calculator.sum(4, 3));
+        assertEquals(-7, calculator.sum(-4, -3));
      }
 }
