07f851030f44c3097d050b54492b3e1a9abdd003
Sampete12 <samuelpete1@gmail.com>
2025-02-17 20:50:54 -0500

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -8,7 +8,5 @@ public class CalculatorTest {
      @Test
      public void testSum() {
           assertEquals(5, calculator.sum(2, 3));
-          assertEquals(-1, calculator.sum(-2, 1));
-          assertEquals(0, calculator.sum(0,0));
      }
 }
