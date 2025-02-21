2880cb3a92d4ed17a41293ba6577f596440b174d
Arthur Rosa <arthur_rosa@student.uml.edu>
2025-02-20 22:58:26 -0500

    testing test java files

--- Chapter08/sample1/src/test/java/acceptance/AcceptanceTest.java
+++ Chapter08/sample1/src/test/java/acceptance/AcceptanceTest.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-/** Acceptance Test */
+/** Acceptance Test test */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:acceptance")
 public class AcceptanceTest { }
