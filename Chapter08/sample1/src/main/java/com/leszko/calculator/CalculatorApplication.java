8b7df8cc66e02c4f4c755886b5a9e09109b28eb6
Arthur Rosa <arthur_rosa@student.uml.edu>
2025-02-20 23:09:18 -0500

    testing src

--- Chapter08/sample1/src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 /**
- * Main Spring test Application.
+ * Main Spring Application.
  */
 @SpringBootApplication
 @EnableCaching
