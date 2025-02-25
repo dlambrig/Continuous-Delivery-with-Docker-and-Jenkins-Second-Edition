cad8fd6c273efaa1935889d31cc7bbf4bdceabe8
Sampete12 <samuelpete1@gmail.com>
2025-02-24 19:46:39 -0500

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -3,6 +3,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * This is a test class for the Calculator class.  It contains various
+ * test methods to verify the functionality of the Calculator.
+ */
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
