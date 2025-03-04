842213f0b08d5c631b5b875a6821d71016a73ede
Sampete12 <samuelpete1@gmail.com>
2025-03-03 22:35:27 -0500

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,47 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('prepare environment') {
+            when {branch 'master'}
+            steps {
+                echo 'all branches'
+                }
+                }
+
+        stage('Code Coverage') {
+            when { branch 'master' }
+            steps {
+                sh """
+                cd Chapter08/sample1
+                ./gradlew codecoverage
+                """
+                }
+        }
+
+        stage('Feature Tests') {
+            when { branch pattern: 'feature.*', result: true }
+            steps {
+                sh """
+                ./gradlew test
+                ./gradlew jacocoTestReport
+                ./gradlew checkstyleTest                
+                """
+            }
+        }
+
+        stage('Fail on Other Branches') {
+            when {
+                not {
+                    anyOf {
+                        branch 'master'
+                        branch pattern: 'feature.*', result: true
+                    }
+                }
+            }
+            steps {
+                error('Branch name does not match allowed patterns.')
+            }
+        }
+    }
+}
