29f48897b785e93f2bcd39bbf2fb968ad7cfab0a
iuliia <iuliiaivakina@yahoo.com>
2025-03-07 22:12:28 -0500

    Increase test coverage to pass Jacoco verification

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,12 +1,33 @@
 package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-
+/**
+ * Tests the Calculator class.
+ */
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
-
+     /**
+     * Tests the sum method of the Calculator.
+     */
      @Test
      public void testSum() {
+          // Test implementation
           assertEquals(5, calculator.sum(2, 3));
+     }
+    /**
+     * Tests the sum method of the Calculator.
+     */
+     @Test
+     public void testSum2() {
+          // Test implementation
+          assertEquals(6, calculator.sum(1,5));
+     }
+     /**
+     * Tests the sum method of the Calculator.
+     */
+     @Test
+     public void testSum3 () {
+          // Test implementation
+          assertEquals(9, calculator.sum(5, 4));
      }
 }
