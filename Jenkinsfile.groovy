dcfac1ce5af755e8a818ae28a2aaad4ed9eedc2b
Sampete12 <samuelpete1@gmail.com>
2025-03-17 21:25:54 -0400

    add homework

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 pipeline {
     agent any
     
@@ -48,3 +49,55 @@ pipeline {
         }
     }
 }
+=======
+pipeline {
+    agent any
+    
+    stages {
+        stage('prepare environment') {
+            when {branch 'master'}
+            steps {
+                echo 'all branches'
+                }
+                }
+
+        stage('Code Coverage') {
+            when { branch 'master' }
+            steps {
+                sh """
+                cd Chapter08/sample1
+                ./gradlew test
+                ./gradlew jacocoTestReport
+                ./gradlew jacocoTestCoverageVerification
+                ./gradlewcheckstyleTest
+                """
+                }
+        }
+
+        stage('Feature Tests') {
+            when { branch 'feature' }
+            steps {
+                sh """
+                ./gradlew test
+                ./gradlew jacocoTestReport
+                ./gradlew checkstyleTest                
+                """
+            }
+        }
+
+        stage('Fail on Other Branches') {
+            when {
+                not {
+                    anyOf {
+                        branch 'master'
+                        branch pattern: '.*feature.*'
+                    }
+                }
+            }
+            steps {
+                error('Branch name does not match allowed patterns.')
+            }
+        }
+    }
+}
+>>>>>>> 8d534815b48bb79f60852657bf9de5ba7a52dda7
