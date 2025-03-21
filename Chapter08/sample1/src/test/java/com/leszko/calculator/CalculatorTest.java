16a46eed66e837361dd10b27463e999d820c815a
Jasp3rGit <Cedric_Urbino@student.uml.edu>
2025-03-21 16:04:38 -0500

    Fix Checkstyle issue in CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,10 +1,15 @@
 package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-
+/**
+ * Unit test for the Calculator class.
+ */
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
+     /**
+      * Tests the sum method of the Calculator class.
+      */
      @Test
      public void testSum() {
           assertEquals(5, calculator.sum(2, 3));
